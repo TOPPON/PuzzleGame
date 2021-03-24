49521f0660f078b79f7b320697b6db6ffded3818
TOPPON <pont717@gmail.com>
2021-03-24 13:16:08 +0900

    【Add】GameManagerの進行部分を追加

--- PuzzleGame/Assets/Scripts/GameManager.cs
+++ PuzzleGame/Assets/Scripts/GameManager.cs
@@ -35,17 +35,19 @@ public class GameManager : MonoBehaviour
                     int line = PuzzleManager.Instance.SelectLine(-1);
                     PuzzleManager.Instance.PutBall(line, PuzzleManager.Instance.nextColor);
                     PuzzleView.Instance.BallThrow(-1, line, PuzzleManager.Instance.nextColor);
+                    ChangeGameState(GameState.BallFallingAnimation);
                 }
                 if (Input.GetKeyDown(KeyCode.RightArrow))
                 {
                     int line=PuzzleManager.Instance.SelectLine(1);
                     PuzzleManager.Instance.PutBall(line,PuzzleManager.Instance.nextColor);
-                    PuzzleView.Instance.BallThrow(1, line, PuzzleManager.Instance.nextColor);
+                    PuzzleView.Instance.BallThrow(1, line, PuzzleManager.Instance.nextColor);//親オブジェクトに引っ付ける作業がまだです。
+                    ChangeGameState(GameState.BallFallingAnimation);
                 }
                 break;
-            case GameState.BallFallingAnimation:
+            case GameState.BallFallingAnimation://枠の当たり判定をいじるところがまだです。
                 break;
-            case GameState.FirstJudge:
+            case GameState.FirstJudge://判定部分も作れていません。判定後落として再判定する部分も作れてないです。
                 break;
             case GameState.RollDirectionSelect:
                 break;
@@ -64,6 +66,9 @@ public class GameManager : MonoBehaviour
             case GameState.LeftRightSelect:
                 PuzzleManager.Instance.ChooseNextColor();
                 PuzzleView.Instance.DisplayNextColor(PuzzleManager.Instance.nextColor);
+                break;
+            case GameState.BallFallingAnimation:
+                PuzzleView.Instance
                 break;
         }
         gameState = nextState;
