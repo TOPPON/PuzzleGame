2a8aebc45f13e9908947f8588cd436e5250a45e1
VivoPCX_pont <pont717@gmail.com>
2022-02-19 23:35:12 +0900

    【Add】ランキングクラスを追加

---
+++ PuzzleGame/Assets/Scripts/RankingManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankingManager : MonoBehaviour
+{
+    public class Ranking
+    {
+        int score;
+        int round;
+        Ranking(int newScore,int newRound)
+        {
+            this.score = newScore;
+            this.round = newRound;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
